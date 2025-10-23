215032c9aff976f7e30febb0e24d8a0ffa964799
Unknownslab999 <hasnatrahim999@gmail.com>
2025-10-23 12:36:27 +0500

    good

---
+++ SCD Lab DevOps/src/third_fourth_lab.java
@@ -0,0 +1,20 @@
+
+public class third_fourth_lab {
+	
+	    public static void main(String[] args) {
+	        // Declare and initialize 5 variables
+	        int a = 10;
+	        int b = 25;
+	        int c = 40;
+	        int d = 55;
+	        int e = 70;
+
+	        // Print the variables
+	        System.out.println("Value of a: " + a);
+	        System.out.println("Value of b: " + b);
+	        System.out.println("Value of c: " + c);
+	        System.out.println("Value of d: " + d);
+	        System.out.println("Value of e: " + e);
+	}
+
+}
