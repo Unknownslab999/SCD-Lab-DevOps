f9d0e8b02b0ef1ab16fb78327fff419aa5b07275
Unknownslab999 <hasnatrahim999@gmail.com>
2025-10-23 12:53:35 +0500

    1st commit

--- SCD Lab DevOps/src/third_fourth_lab.java
+++ SCD Lab DevOps/src/third_fourth_lab.java
@@ -8,6 +8,7 @@ public class third_fourth_lab {
 	        int c = 40;
 	        int d = 55;
 	        int e = 70;
+	        int g = 90;
 
 	        // Print the variables
 	        System.out.println("Value of a: " + a);
@@ -15,6 +16,7 @@ public class third_fourth_lab {
 	        System.out.println("Value of c: " + c);
 	        System.out.println("Value of d: " + d);
 	        System.out.println("Value of e: " + e);
+	        System.out.println("Value of g: " + g);
 	}
 
 }
